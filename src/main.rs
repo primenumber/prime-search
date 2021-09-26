67324ee5592c121c5ba07a405613c15a148fe3ee
primenumber <prime@kmc.gr.jp>
2021-09-27 06:20:26 +0900

    Implement count_primes

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 extern crate iron;
 extern crate router;
 extern crate staticfile;
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 extern crate urlencoded;
 
+use iron::mime::Mime;
 use iron::prelude::*;
 use iron::status;
-use staticfile::Static;
-use router::Router;
 use mount::Mount;
-use std::str::FromStr;
-use std::path::Path;
-use urlencoded::UrlEncodedBody;
-use iron::mime::Mime;
+use router::Router;
+use staticfile::Static;
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+use std::str::FromStr;
+use urlencoded::UrlEncodedBody;
 
 const SQRTN: usize = 4096;
 const N: usize = SQRTN * SQRTN;
@@ -30,56 +31,70 @@ lazy_static! {
     };
     static ref PARTIAL_SUM: Vec<usize> = {
         let m = COUNT.len();
-        let mut res = vec![0; m+1];
+        let mut res = vec![0; m + 1];
         for i in 0usize..m {
-            res[i+1] = res[i] + COUNT[i];
+            res[i + 1] = res[i] + COUNT[i];
         }
         res
     };
 }
 
 fn prime_list() -> Vec<usize> {
-    let mut is_prime = vec![true; N+1];
+    let mut is_prime = vec![true; N + 1];
     is_prime[0] = false;
     is_prime[1] = false;
-    for i in 2usize..(N+1) {
-        if i*i > N {
+    for i in 2usize..(N + 1) {
+        if i * i > N {
             break;
         }
         if is_prime[i] {
-            let mut j = i*i;
+            let mut j = i * i;
             while j <= N {
                 is_prime[j] = false;
                 j += i;
             }
         }
     }
-    is_prime.iter().enumerate().filter_map(|(i,is_p)|
-                                           if *is_p {
-                                               return Some(i);
-                                           } else {
-                                               return None;
-                                           }).collect()
+    is_prime
+        .iter()
+        .enumerate()
+        .filter_map(|(i, is_p)| {
+            if *is_p {
+                return Some(i);
+            } else {
+                return None;
+            }
+        })
+        .collect()
 }
 
 fn prime_list_range(begin: usize, size: usize) -> Vec<usize> {
     let mut is_prime = vec![true; size];
     for i in PRIME_LIST.iter() {
-        if i*i >= begin+size {
+        if i * i >= begin + size {
             break;
         }
         let mut j = (i - (begin % i)) % i;
         while j < size {
-            is_prime[j] = false;
+            if begin + j > *i {
+                is_prime[j] = false;
+            }
             j += i;
         }
     }
-    is_prime.iter().enumerate().filter_map(|(i,is_p)|
-                                           if *is_p {
-                                               return Some(begin+i);
-                                           } else {
-                                               return None;
-                                           }).collect()
+    is_prime
+        .iter()
+        .enumerate()
+        .filter_map(|(i, is_p)| {
+            if begin + i <= 1 {
+                return None;
+            } else if *is_p {
+                return Some(begin + i);
+            } else {
+                return None;
+            }
+        })
+        .collect()
 }
 
 fn main() {
@@ -89,6 +104,7 @@ fn main() {
     let mut router = Router::new();
 
     router.post("/nth_prime", post_nth_prime, "nth_prime");
+    router.post("/prime_count", post_prime_count, "prime_count");
 
     let mut mount = Mount::new();
     mount.mount("/", Static::new(Path::new("static/")));
@@ -102,7 +118,9 @@ fn main() {
 
     let port = 3210;
     println!("Serving on http://localhost:{}...", port);
-    Iron::new(mount).http(format!("localhost:{}", port)).unwrap();
+    Iron::new(mount)
+        .http(format!("localhost:{}", port))
+        .unwrap();
 }
 
 fn post_nth_prime(request: &mut Request) -> IronResult<Response> {
@@ -113,26 +131,33 @@ fn post_nth_prime(request: &mut Request) -> IronResult<Response> {
         Err(e) => {
             response.set_mut(status::BadRequest);
             response.set_mut("application/json".parse::<Mime>().unwrap());
-            response.set_mut(format!(r#"{{"status":"error", "message":"Error parsing form data: {:?}"}}\n"#, e));
+            response.set_mut(format!(
+                r#"{{"status":"error", "message":"Error parsing form data: {:?}"}}\n"#,
+                e
+            ));
             return Ok(response);
         }
-        Ok(map) => map
+        Ok(map) => map,
     };
 
     let unparsed_numbers = match form_data.get("n") {
         None => {
             response.set_mut(status::BadRequest);
             response.set_mut("application/json".parse::<Mime>().unwrap());
-            response.set_mut(format!(r#"{{"status":"error", "message":"Form data has no 'n' parameter"}}\n"#));
+            response.set_mut(format!(
+                r#"{{"status":"error", "message":"Form data has no 'n' parameter"}}\n"#
+            ));
             return Ok(response);
         }
-        Some(nums) => nums
+        Some(nums) => nums,
     };
 
     if unparsed_numbers.len() != 1 {
         response.set_mut(status::BadRequest);
         response.set_mut("application/json".parse::<Mime>().unwrap());
-        response.set_mut(format!(r#"{{"status":"error", "message":"number of 'n' must be 1"}}"#));
+        response.set_mut(format!(
+            r#"{{"status":"error", "message":"number of 'n' must be 1"}}"#
+        ));
         return Ok(response);
     }
 
@@ -140,16 +165,22 @@ fn post_nth_prime(request: &mut Request) -> IronResult<Response> {
         Err(_) => {
             response.set_mut(status::BadRequest);
             response.set_mut("application/json".parse::<Mime>().unwrap());
-            response.set_mut(format!(r#"{{"status":"error", "message":"Value for 'n' parameter not a number: {}"}}"#, unparsed_numbers[0]));
+            response.set_mut(format!(
+                r#"{{"status":"error", "message":"Value for 'n' parameter not a number: {}"}}"#,
+                unparsed_numbers[0]
+            ));
             return Ok(response);
         }
-        Ok(x) => x
+        Ok(x) => x,
     };
 
     if number > *PARTIAL_SUM.iter().last().unwrap() {
         response.set_mut(status::BadRequest);
         response.set_mut("application/json".parse::<Mime>().unwrap());
-        response.set_mut(format!(r#"{{"status":"error", "message":"Too Large number: {}"}}"#, number));
+        response.set_mut(format!(
+            r#"{{"status":"error", "message":"Too Large number: {}"}}"#,
+            number
+        ));
         return Ok(response);
     }
 
@@ -184,8 +215,85 @@ fn nth_prime(x: usize) -> usize {
     if lo > 0 {
         let v = prime_list_range(N * lo, N);
         let rem = x - PARTIAL_SUM[lo];
-        v[rem-1]
+        v[rem - 1]
     } else {
-        PRIME_LIST[x-1]
+        PRIME_LIST[x - 1]
     }
+}
+
+fn post_prime_count(request: &mut Request) -> IronResult<Response> {
+    println!("post_prime_count");
+    let mut response = Response::new();
+
+    let form_data = match request.get_ref::<UrlEncodedBody>() {
+        Err(e) => {
+            response.set_mut(status::BadRequest);
+            response.set_mut("application/json".parse::<Mime>().unwrap());
+            response.set_mut(format!(
+                r#"{{"status":"error", "message":"Error parsing form data: {:?}"}}\n"#,
+                e
+            ));
+            return Ok(response);
+        }
+        Ok(map) => map,
+    };
+
+    let unparsed_numbers = match form_data.get("n") {
+        None => {
+            response.set_mut(status::BadRequest);
+            response.set_mut("application/json".parse::<Mime>().unwrap());
+            response.set_mut(format!(
+                r#"{{"status":"error", "message":"Form data has no 'n' parameter"}}\n"#
+            ));
+            return Ok(response);
+        }
+        Some(nums) => nums,
+    };
+
+    if unparsed_numbers.len() != 1 {
+        response.set_mut(status::BadRequest);
+        response.set_mut("application/json".parse::<Mime>().unwrap());
+        response.set_mut(format!(
+            r#"{{"status":"error", "message":"number of 'n' must be 1"}}"#
+        ));
+        return Ok(response);
+    }
+
+    let number = match usize::from_str(&unparsed_numbers[0]) {
+        Err(_) => {
+            response.set_mut(status::BadRequest);
+            response.set_mut("application/json".parse::<Mime>().unwrap());
+            response.set_mut(format!(
+                r#"{{"status":"error", "message":"Value for 'n' parameter not a number: {}"}}"#,
+                unparsed_numbers[0]
+            ));
+            return Ok(response);
+        }
+        Ok(x) => x,
+    };
+
+    if number > COUNT.len() * N {
+        response.set_mut(status::BadRequest);
+        response.set_mut("application/json".parse::<Mime>().unwrap());
+        response.set_mut(format!(
+            r#"{{"status":"error", "message":"Too Large number: {}"}}"#,
+            number
+        ));
+        return Ok(response);
+    }
+
+    let p = prime_count(number);
+
+    response.set_mut(status::Ok);
+    response.set_mut("application/json".parse::<Mime>().unwrap());
+    response.set_mut(format!(r#"{{"status":"ok", "result":{}}}"#, p));
+    println!(r#"{{"status":"ok", "result":{}}}"#, p);
+
+    Ok(response)
+}
+
+fn prime_count(x: usize) -> usize {
+    let row = x / N;
+    let col = x % N;
+    PARTIAL_SUM[row] + prime_list_range(N * row, col + 1).len()
 }
--- static/app.js
+++ static/app.js
@@ -16,3 +16,22 @@ function get_nth_prime() {
     }
   });
 }
+
+async function prime_count(n) {
+  const response = await fetch('./api/prime_count', {
+    method: 'POST',
+    body: `n=${n}`
+  });
+  return response.json();
+}
+
+function count_primes() {
+  const n = parseInt(document.getElementById("n").value);
+  prime_count(n).then(data => {
+    if (data["status"] === "ok") {
+      document.getElementById("result").textContent = data["result"];
+    } else {
+      document.getElementById("result").textContent = "Error: " + data["message"];
+    }
+  });
+}
